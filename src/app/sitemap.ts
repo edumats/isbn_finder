ccba414ed13d02c51d17a4c6f2ef2367b748aa19
edumats <eduardo.mats@gmail.com>
2023-08-28 15:08:48 -0300

    Updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,6 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://buscaisb.com.br',
-      lastModified: new Date(),
-    },
-    {
       url: 'https://www.buscaisbn.com.br',
       lastModified: new Date(),
     },
